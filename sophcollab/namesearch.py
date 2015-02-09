201875264e3f555b6993d86a0690ff550156d0e4
marcochiesa <marco_chiesa@hotmail.com>
2015-02-09 17:42:52 -0600

    Cleanup.

--- sophcollab/namesearch.py
+++ sophcollab/namesearch.py
@@ -1,4 +1,5 @@
 from . import pubmed
+#import pubmed
 import re
 
 class NameParser(object):
@@ -13,24 +14,39 @@ class NameParser(object):
             raise Exception("null name parts")
 
         # Clean name
-        # Strip leading and trailing whitespace
-        name_parts = name_parts.strip()
-        # Condense multiple whitespace characters to one space
-        name_parts = self.WHITESPACE_PATTERN.sub(self.SPACE, name_parts)
+        name_parts = self.collapse_string(name_parts)
 
-        last = ""
-        rest = name_parts
         match = self.LAST_NAME_FIRST_PATTERN.match(name_parts)
         if match is not None:
             last = match.group(1).strip()
             rest = match.group(2).strip()
+        else:
+            arr = name_parts.split(self.SPACE)
+            last = arr[-1]
+            rest = self.SPACE.join(arr[:-1])
 
-        self.name = rest
-        if last:
-            self.name += " " + last
+        arr = rest.split(self.SPACE)
+        first = arr[0]
+        rest = self.SPACE.join(arr[1:])
 
-    def get_name(self):
+        self.first_name = first
+        self.middle_name = rest
+        self.last_name = last
+        self.name = self.collapse_string(self.SPACE.join([first, rest, last]))
+
+    def full_name(self):
         return self.name
+
+    def short_name(self):
+        return self.collapse_string(self.SPACE.join([self.first_name, self.last_name]))
+        
+    @classmethod
+    def collapse_string(cls, s):
+        # Strip leading and trailing whitespace
+        s = s.strip()
+        # Condense multiple whitespace characters to one space
+        s = cls.WHITESPACE_PATTERN.sub(cls.SPACE, s)
+        return s
 
 class PubmedNameSearch(object):
 
@@ -38,8 +54,159 @@ class PubmedNameSearch(object):
     def __init__(self, name_list):
         if name_list is None:
             raise Exception("null name list")
-        self.name_list = [NameParser(x.strip()).get_name() for x in name_list]
+        self.name_list = [NameParser(x.strip()).name for x in name_list]
+        self.author_container = AuthorContainer()
 
     def article_counts_by_name(self, progress_observer = None):
-        m = pubmed.Runner(self.name_list).run(progress_observer)
+        m = pubmed.ArticleCountByNameRunner(self.name_list).run(self.author_container, progress_observer)
         return {k:len(v) for k, v in m.items()}
+
+    def build_author_graph(self, progress_observer = None):
+        m = pubmed.BuildAuthorGraphRunner(self.name_list).run(self.author_container, progress_observer)
+        return m
+
+    def collaboration_report(self, progress_observer = None):
+        author_graph = self.build_author_graph(progress_observer)
+        data = {}
+        for name, author in author_graph.items():
+            article_count = author.get_article_count()
+            collaborators = author.get_collaborators()
+            collaborator_count = len(collaborators)
+            num_soph = 0
+            num_uab = 0
+            num_other = 0
+            num_unknown = 0
+            for collaborator in collaborators:
+                if author.is_soph():
+                    num_soph += 1 
+                if author.is_uab():
+                    num_uab += 1
+                if author.is_other():
+                    num_other += 1
+                if author.is_unknown():
+                    num_unknown += 1
+            data[name] = [article_count, collaborator_count, num_uab, num_soph, num_other, num_unknown]
+
+        labels = ["Num of Articles", "Num of Collaborators", "Num from UAB", "Num from UAB SoPH", "Num from Other Institutions", "Num with Unknown Affiliation"]
+        return {"data": data, "labels": labels}
+
+class AuthorContainer(object):
+    def __init__(self):
+        self.authors = {}
+
+    def get_author(self, name):
+        parser = NameParser(name)
+        full = parser.full_name()
+        short = parser.short_name()
+        if full in self.authors:
+            author = self.authors[full]
+        elif short in self.authors:
+            author = self.authors[short]
+        else:
+            author = Author(full)
+            self.authors[full] = author
+
+        return author
+
+    def get_authors(self):
+        return dict(self.authors)
+
+
+class Author(object):
+    def __init__(self, name):
+        self.name = name
+        self.articles = set()
+        self.affiliation = ""
+
+    def get_article_count(self):
+        return len(self.articles)
+
+    def get_articles(self):
+        return set(self.articles)
+
+    def add_article(self, article):
+        self.articles.add(article)
+
+    def set_affiliation(self, affiliation):
+        self.affiliation = affiliation
+
+    def get_collaborators(self):
+        collaborators = set()
+        for article in self.articles:
+            for author in article.get_authors():
+                if author != self:
+                    collaborators.add(author)
+        return collaborators
+
+    def is_uab(self):
+        return "University of Alabama at Birmingham" in self.affiliation
+
+    def is_soph(self):
+        return "School of Public Health" in self.affiliation
+
+    def is_other(self):
+        return self.affiliation and not self.is_soph() and not self.is_uab()
+
+    def is_unknown(self):
+        return not self.affiliation
+
+
+class Article(object):
+    def __init__(self, article_id, title, abstract):
+        self.article_id = article_id
+        self.title = title
+        self.abstract = abstract
+        self.authors = set()
+
+    def add_author(self, author):
+        author.add_article(self)
+        self.authors.add(author)
+
+    def get_author_count(self):
+        return len(self.authors)
+
+    def get_authors(self):
+        return set(self.authors)
+
+def main():
+    pubmed.DEBUG = True
+
+    """
+    article_counts = PubmedNameSearch(["Stephen Mennemeyer"]).article_counts_by_name()
+    for name, count in article_counts.items():
+        print(name, count)
+    """
+
+#name_search = PubmedNameSearch(["Allison DB"])
+    name_search = PubmedNameSearch(["Stephen Mennemeyer"])
+    author_list = name_search.build_author_graph()
+#for name, author in name_search.author_container.get_authors().items():
+    for name, author in author_list.items():
+        print(author.name + (" - UAB" if author.is_uab() else ""))
+        if author.affiliation:
+            print(author.affiliation)
+        collaborators = author.get_collaborators()
+        print("  collaborator count: " + str(len(collaborators)))
+        for collaborator in collaborators:
+            print("    " + collaborator.name)
+        article_count = author.get_article_count()
+        print("  article count: " + str(article_count))
+        if article_count > 0:
+            print("  articles:")
+        for article in author.get_articles():
+            print("    " + article.article_id + " - " + article.title)
+            author_count = article.get_author_count()
+            print("      author count: " + str(author_count))
+            for collaborator in article.get_authors():
+                print("      " + collaborator.name, end="")
+                if collaborator == author:
+                    print(" (author)")
+                elif collaborator.is_soph():
+                    print(" (SoPH)")
+                elif collaborator.is_uab():
+                    print(" (UAB)")
+                else:
+                    print("(N/A)")
+
+if __name__ == '__main__':
+    main()
--- sophcollab/pubmed.py
+++ sophcollab/pubmed.py
@@ -1,11 +1,22 @@
-from suds.client import Client
+import urllib
 import urllib.parse
+import urllib.request
+import xml.etree.ElementTree
 import logging
 import time
+#import namesearch
+from . import namesearch
+
+DEBUG = False
+TRACE = False
 
 logging.basicConfig(level=logging.WARN)
 logging.getLogger('suds.client').setLevel(logging.WARN)
 logging.getLogger('suds.transport').setLevel(logging.WARN)
+
+URL_SEPARATOR = "/"
+QUERY_SEPARATOR = "?"
+PUBMED_DB = "pubmed"
 
 class Runner(object):
     PROGRESS_UPDATE_BATCH_SIZE = 1
@@ -15,12 +26,12 @@ class Runner(object):
             raise Exception("null name list")
         self.names = names
 
-    def run(self, progress_observer = None):
+    def run(self, author_container = None, progress_observer = None):
         results = {}
         loop = 0
         for name in self.names:
             loop += 1
-            results[name] = ESearch().search_author(name).item_list()
+            results[name] = self.run_name(name, author_container)
             if loop >= self.PROGRESS_UPDATE_BATCH_SIZE and progress_observer is not None:
                 progress_observer(len(results), len(self.names))
                 loop = 0
@@ -28,29 +39,91 @@ class Runner(object):
             time.sleep(0.5)
         return results
 
+    def run_name(self, name, author_container):
+        pass
+
+# Builds dictionary of name (string) to list of article IDs 
+class ArticleCountByNameRunner(Runner):
+    def run_name(self, name, author_container):
+        return ESearch().search_author(name).items()
+
+# Builds name (string) to Author dictionary
+class BuildAuthorGraphRunner(Runner):
+    PUBMED_ARTICLE_SET_TAG = "PubmedArticleSet"
+    PUBMED_ARTICLE_TAG = "PubmedArticle"
+    PMID_TAG = "PMID"
+    ARTICLE_TAG = "Article"
+    ARTICLE_TITLE_TAG = "ArticleTitle"
+    ABSTRACT_TAG = "Abstract"
+    ABSTRACT_TEXT_TAG = "AbstractText"
+    AUTHOR_LIST_TAG = "AuthorList"
+    AUTHOR_TAG = "Author"
+    LAST_NAME_TAG = "LastName"
+    FORE_NAME_TAG = "ForeName"
+    AFFILIATION_INFO_TAG = "AffiliationInfo"
+    AFFILIATION_TAG = "Affiliation"
+
+    def run_name(self, name, author_container):
+        author = author_container.get_author(name)
+        article_id_list = ESearch().search_author(author.name).items()
+        for article_id in article_id_list:
+            article_xml = EFetch().search_article(article_id)
+            self.parse_xml_article(article_xml, author_container)
+        return author
+
+    def parse_xml_article(self, article_set_xml, author_container):
+        xml_root = xml.etree.ElementTree.fromstring(article_set_xml) #root element
+        if xml_root.tag != self.PUBMED_ARTICLE_SET_TAG:
+            raise Exception("invalid xml content: " + article_xml)
+
+        for pubmed_article_tag in xml_root.findall(self.PUBMED_ARTICLE_TAG):
+            pmid_tag = pubmed_article_tag.find(".//"+self.PMID_TAG)
+            article_id = pmid_tag.text
+            article_tag = pubmed_article_tag.find(".//"+self.ARTICLE_TAG)
+            title = article_tag.find(self.ARTICLE_TITLE_TAG).text
+            abstract = ""
+            abstract_tag = article_tag.find(self.ABSTRACT_TAG)
+            if abstract_tag is not None:
+                for abstract_text_tag in abstract_tag.findall(self.ABSTRACT_TEXT_TAG):
+                    abstract += abstract_text_tag.text
+            article = namesearch.Article(article_id, title, abstract)
+            for author_tag in article_tag.find(self.AUTHOR_LIST_TAG).findall(self.AUTHOR_TAG):
+                fore_name_tag = author_tag.find(self.FORE_NAME_TAG)
+                first_name = fore_name_tag.text if fore_name_tag is not None else ""
+                last_name_tag = author_tag.find(self.LAST_NAME_TAG)
+                last_name = last_name_tag.text if last_name_tag is not None else ""
+                author = author_container.get_author(first_name + " " + last_name)
+                affiliation_tag = author_tag.find("./" + self.AFFILIATION_INFO_TAG + "/" + self.AFFILIATION_TAG)
+                if affiliation_tag is not None and not author.affiliation:
+                    author.set_affiliation(affiliation_tag.text)
+                article.add_author(author)
+            
+            
 class Entrez(object):
-    ENTREZ_WSDL_URL = "http://www.ncbi.nlm.nih.gov/soap/v2.0/eutils.wsdl"
-    ENTREZ_ENDPOINT_URL = "http://eutils.ncbi.nlm.nih.gov/soap/v2.0/soap_adapter_2_0.cgi"
+    ENTREZ_HOST = "eutils.ncbi.nlm.nih.gov"
+    ENTREZ_PATH = "entrez/eutils"
+    ENTREZ_SCHEME = "http://"
 
     @classmethod
-    def get_wsdl_url(cls):
-        return cls.ENTREZ_WSDL_URL
+    def get_endpoint_url(cls, script):
+        return cls.ENTREZ_SCHEME + cls.ENTREZ_HOST + URL_SEPARATOR + cls.ENTREZ_PATH + URL_SEPARATOR + str(script)
 
+    # args should be a dictionary of query string parameters
     @classmethod
-    def get_endpoint_url(cls):
-        return cls.ENTREZ_ENDPOINT_URL
-
-    @classmethod
-    def build_client(cls):
-        return Client(cls.get_wsdl_url(), location=cls.get_endpoint_url())
-
-    @classmethod
-    def print_service_methods(cls):
-        client = Client(cls.get_wsdl_url())
-        print(cls.build_client())
+    def query_endpoint(cls, query_url, args):
+        global DEBUG
+        global TRACE
+        data = urllib.parse.urlencode(args)
+        if DEBUG:
+            print("***query data: " + data)
+        response = urllib.request.urlopen(query_url + QUERY_SEPARATOR + data)
+        result = response.read()
+        if TRACE:
+            print(result)
+        return result
 
 class ESearch(object):
-    PUBMED_DB = "pubmed"
+    ESEARCH_SCRIPT = "esearch.fcgi"
 
     def __init__(self):
         pass
@@ -59,71 +132,86 @@ class ESearch(object):
         return self.search(name, "author")
 
     def search(self, termy, fieldy):
-        client = Entrez.build_client()
+        url = Entrez.get_endpoint_url(self.ESEARCH_SCRIPT)
         args = dict(
-            db = self._escape(self.PUBMED_DB),
-            term = self._escape(termy),
-#WebEnv = self._escape(""),
-#QueryKey = self._escape("0"),
-#usehistory = self._escape("n"),
-#tool = self._escape(""),
-#email = self._escape(""),
-            field = self._escape(fieldy),
-#reldate = self._escape("1000"),
-#mindate = self._escape("2000"),
-#maxdate = self._escape("2014"),
-#datetype = self._escape("pdat"),
-#RetStart = self._escape("0"),
-            RetMax = self._escape("1000"),
-#rettype = self._escape("uilist"),
-            sort = self._escape("pub date")
+            db = PUBMED_DB,
+            term = termy,
+#WebEnv = "",
+#QueryKey = "0",
+#usehistory = "n",
+#tool = "",
+#email = "",
+            field = fieldy,
+#reldate = "1000",
+#mindate = "2000",
+#maxdate = "2014",
+#datetype = "pdat",
+#RetStart = "0",
+            RetMax = "1000",
+#rettype = "uilist",
+            sort = "pub date"
             )
-        result=client.service.run_eSearch(**args)
-#print(result)
-        return ESearchResults(result)
 
-    @classmethod
-    def _escape(cls, str):
-        return urllib.parse.quote_plus(str)
+        return ESearchResults(Entrez.query_endpoint(url, args))
+
 
 class ESearchResults(object):
-    def __init__(self, suds_result):
-        if suds_result is None:
+
+    RESULT_TAG = "eSearchResult"
+    COUNT_TAG = "Count"
+    RET_START_TAG = "RetStart"
+    RET_MAX_TAG = "RetMax"
+    QUERY_TRANSLATION_TAG = "QueryTranslation"
+    ID_LIST_TAG = "IdList"
+    ID_TAG = "Id"
+
+    def __init__(self, xml_text):
+        if xml_text is None:
             raise Exception("null suds result")
-        self.suds_result = suds_result
+        self.xml = xml.etree.ElementTree.fromstring(xml_text) #root element
+        if self.xml.tag != self.RESULT_TAG:
+            raise Exception("invalid response: " + xml_text)
 
     # Returns the total number of results in the database regardless of how many
     # results were returned for this service query (returned results subject to
     # RetMax value in request)
     def total(self):
-        return int(str(self.suds_result.Count))
+        return int(str(self.xml.find(self.COUNT_TAG).text))
 
     def pagination_start(self):
-        return int(str(self.suds_result.RetStart))
+        return int(str(self.xml.find(self.RET_START_TAG).text))
 
     def pagination_size(self):
-        return int(str(self.suds_result.RetMax))
+        return int(str(self.xml.find(self.RET_MAX_TAG).text))
 
     def query_string(self):
-        return str(self.suds_result.QueryTranslation)
+        return str(self.xml.find(self.QUERY_TRANSLATION_TAG).text)
 
-    def translation_set(self):
-        return str(self.suds_result.TranslationSet)
-
-    def print_translation_stack(self):
-        return print(self.suds_result.TranslationStack)
-
-    # Returns a generator object to iterate through the translated results
-    # (string entries instead of objects representing xml text element)
+    # Returns a list of string values (pubmed database IDs)
     def items(self):
-        lst = []
+        result = []
         if self.total() > 0:
-            lst = self.suds_result.IdList.Id
-        return (str(x) for x in lst)
-
-    # Returns a translated, populated list of the results
-    def item_list(self):
-        return list(self.items())
+            lst = self.xml.find(self.ID_LIST_TAG).findall(self.ID_TAG)
+            result = [x.text for x in lst]
+        return result
 
     def __str__(self):
         return "query: '%s' - total results: %d" % (self.query_string(), self.total())
+
+
+class EFetch(object):
+    EFETCH_SCRIPT = "efetch.fcgi"
+
+    def __init__(self):
+        pass
+
+    def search_article(self, article_id):
+        url = Entrez.get_endpoint_url(self.EFETCH_SCRIPT)
+        args = dict(
+            db = PUBMED_DB,
+            id = article_id,
+            retmode = "xml"
+            )
+
+        return Entrez.query_endpoint(url, args)
+
